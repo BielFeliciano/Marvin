00a7106b90fa773f8f1a9b52bc0ee75b0f8b20a5
BielFeliciano <59872915+BielFeliciano@users.noreply.github.com>
2020-01-23 10:00:02 -0300

    ativ2-3

--- fase02/ex03/maiusculo.js
+++ fase02/ex03/maiusculo.js
@@ -1,3 +1,5 @@
 function maiusculo(palavra){
-    return("maiusculo".substring(0, 3).toUpperCase() + "maiusculo".substring(3, 9).toLowerCase());
+    return palavra.substring(0,3).toUpperCase() + palavra.substring(3,10).toLowerCase()
 }
+
+
