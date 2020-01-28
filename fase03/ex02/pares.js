eeef5862d5459bddf057281b6433a0e2080a76b4
BielFeliciano <59872915+BielFeliciano@users.noreply.github.com>
2020-01-28 11:41:12 -0300

    Carol lanso a braba

---
+++ fase03/ex02/pares.js
@@ -0,0 +1,10 @@
+function pares(num1, num2){
+    var numero = num1, num2
+    var total = numero % 2;
+    if ( total == 0 ){
+        return ("Os numeros são pares")
+    }else {
+        return ("os numeros não são pares")
+    }
+}
+
