e18cf04ff9542ef9cfb24f674070fd9906d9c7c3
BielFeliciano <59872915+BielFeliciano@users.noreply.github.com>
2020-02-11 10:37:36 -0300

    003

--- fase04/ex00/entreNumeros.js
+++ fase04/ex00/entreNumeros.js
@@ -1,7 +1,12 @@
-function entreNumeros(min, max){
-    var c = min
-    while (c <= max){
-        console.log(c)
-        c++
-    }
-}
+function por42(num1, num2){
+    var result = null
+    while ( num1 <= num2){
+        if ( num1 % 42 === 0){
+            return Math.max(num1+42)
+        }
+        num1++
+    } if (!result)
+    console.log("Não encontrado")
+    return false
+} 
+
