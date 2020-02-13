e4b51e23041ee6801e1c34efa9d1cee35cfe8809
BielFeliciano <59872915+BielFeliciano@users.noreply.github.com>
2020-02-13 11:57:32 -0300

    5.1

--- fase05/ex01/penultimo.js
+++ fase05/ex01/penultimo.js
@@ -1,4 +1,15 @@
 function penultimo(array){
     var paes = array 
-    return (paes.slice(-2, -1)).toString()
+    while (array === 'string'){
+        return (array)
+        array++
+    } while ( array === 'number'){
+        return (array)
+        array++
+    }
+    while(array === 'boolean'){
+        return (array)
+        array++
+    }
+    return (paes.slice(-2, -1)).sort()
 }
