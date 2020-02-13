0fc08d477df46dd5460f96ae050578a58745bad7
BielFeliciano <59872915+BielFeliciano@users.noreply.github.com>
2020-02-13 08:14:20 -0300

    fizzbuzz

--- fase04/ex00/entreNumeros.js
+++ fase04/ex00/entreNumeros.js
@@ -1,7 +1,15 @@
-function entreNumeros(min, max){
-    var c = min
-    while ( c <= max){
-    console.log(c)
+function fizzbuzz(num1, num2){
+    var c = num1
+    while (c <= num2){
+        if (c % 3 === 0 && c % 5 === 0){
+        console.log(c  + " FizzBuzz")
+    } else if ( c % 5 === 0){
+        console.log(c + " Buzz")
+    } else if (c % 3 === 0 ){
+        console.log(c + " Fizz")
+    } else {
+        console.log(c)
+    }
     c++
-   }
+    }
 }
